8e7217538c1209b14a963c752bf5a7dcf6d332e5
mdashikalikhan <khan.ashik@gmail.com>
2025-07-13 16:37:12 +0600

    ,

--- src/app/components/client/client.ts
+++ src/app/components/client/client.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-client',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './client.html',
   styleUrl: './client.css'
 })
 export class Client {
+  clientObject : Client = new Client();
 
+  clients: Client[] = [];
 }
---
+++ src/app/model/class/Client.ts
@@ -0,0 +1,28 @@
+export class Client{
+    clientId: number;
+    contactPersonName: string;
+    companyName:string;
+    address:string;
+    city:string;
+    pinCode:string;
+    state:string;
+    employeeStrength:number;
+    gstNo:string;
+    contactNo:string;
+    regNo:string;
+
+    constructor(){
+        this.clientId=0;
+        this.companyName="";
+        this.companyName="";
+        this.contactPersonName="";
+        this.address="";
+        this.city="";
+        this.pinCode="";
+        this.state="";
+        this.employeeStrength=0;
+        this.gstNo="";
+        this.contactNo="";
+        this.regNo="";
+    }
+}
